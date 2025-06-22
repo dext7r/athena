dd8e82636c96c13b5e5923ebc8234486b5d1e7a2
h7ml <h7ml@qq.com>
2025-06-23 00:00:12 +0800

    build(dev): 配置 Tailwind 插件并更新导入路径
    - 在 dev.ts 中导入并配置 Tailwind 插件
    - 更新 deno.json 中的导入路径，使用 jsr 而不是 npm

--- dev.ts
+++ dev.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
+import { tailwind } from "@fresh/plugin-tailwind";
 import { initDevelopmentEnvironment } from "@utils/env.ts";
 import { Builder } from "fresh/dev";
 import { app } from "./main.ts";
@@ -10,8 +11,8 @@ await initDevelopmentEnvironment();
 // Pass development only configuration here
 const builder = new Builder({ target: "safari12" });
 
-// TODO: Configure Tailwind plugin for development
-// tailwind(builder, app, {});
+// Configure Tailwind plugin for development
+tailwind(builder, app, {});
 
 // Create optimized assets for the browser when
 // running `deno run -A dev.ts build`
